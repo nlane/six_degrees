6e920ff0a789f24427da81d16f00e2994a33e1c6
Natalie Lane <nlane@udel.edu>
2015-07-08 21:52:51 +0000

    testing things

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ router.get('/:FirstName1/:LastName1/:FirstName2/:LastName2', function(req, res){
   var Actors
   con.query("Select ActorId from Actors where (FirstName = ' " + req.params.FirstName1 + "' and LastName = '" + req.params.LastName1 + "') or (FirstName= ' " + req.params.FirstName2 + "' and LastName= '" + req.params.LastName2 + "')", function(err, rows, fields){
     if (!err){
-      Actors = rows;
+      Actors = rows.ActorId;
       res.send(rows);
     } else {
       res.json({error: "Make sure you typed the name correctly! :)"});
