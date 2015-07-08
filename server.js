87ee17d749879ba10de9eed7baac087bfa201047
Natalie Lane <nlane@udel.edu>
2015-07-08 15:14:44 +0000

    updat

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var http = require('http');
 var express = require('express');
 var mysql = require('mysql');
-var PythonShell = require('python-shell');
+// var PythonShell = require('python-shell');
 
 var router = express();
 var server = http.createServer(router);
 
 var USERNAME = "nlane";
-var PORT = "47461";
+var PORT = "47433";
 var PWD = USERNAME.split('').reverse().join('');
 
 var con = mysql.createConnection({
@@ -35,7 +35,7 @@ con.connect();
 
 
 router.get('/', function(req, res){
-  res.send("Welcome to Six Degrees of Kevin Bacon!")
+  res.send("Welcome to Six Degrees of Kevin Bacon!");
 });
 
 router.get('/test', function(req, res){
